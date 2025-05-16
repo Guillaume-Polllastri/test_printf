064e1c6d8a0233e72f796e786acf6d632c0b5d6e
Guillaume Pollastri <gpollast@f6r10s6.clusters.42paris.fr>
2025-05-16 17:28:03 +0200

    re correction %d %i

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:56:03 by gpollast          #+#    #+#             */
-/*   Updated: 2025/05/16 17:23:54 by gpollast         ###   ########.fr       */
+/*   Updated: 2025/05/16 17:27:29 by gpollast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ int	main(void)
 	printf("%d\n", printf("Resulat s : %r%s %s\n", "toto", "tata"));
 	ft_printf("%d\n", ft_printf("Resultat c : %c\n", 'a'));
 	ft_printf("%d\n", ft_printf("Resultat d : %d\n", 1));
+	ft_printf("%d\n", ft_printf("Resultat i : %i\n", 1));
 }
